8e856ba29cad57d0fa85d4d0e3703d64131e4e1d
Vishal Kuo <vishalkuo@hotmail.com>
2016-04-10 12:46:12 -0400

    Refactor to config.ni

--- python/finance.py
+++ python/finance.py
@@ -1,19 +1,26 @@
 import requests
+import ConfigParser
 base_url = 'http://ichart.finance.yahoo.com/table.csv'
+config = ConfigParser.SafeConfigParser()
+global_config_header = 'global'
+config.read('config.ini')
+ticker = config.get(global_config_header, 'ticker')
+month_to = config.get(global_config_header, 'month_to')
+day_to = config.get(global_config_header, 'day_to')
+year_to = config.get(global_config_header, 'year_to')
+month_from = config.get(global_config_header, 'month_from')
+day_from = config.get(global_config_header, 'day_from')
+year_from = config.get(global_config_header, 'year_from')
 payload = {
-	's': 'VTI',
-	'd': '0',
-	'e': '28',
-	'f': '2010',
+	's': ticker,
+	'd': month_to,
+	'e': day_to,
+	'f': year_to,
 	'g': 'd',
-	'a': '3',
-	'b': '12',
-	'c': '2008',
+	'a': month_from,
+	'b': day_from,
+	'c': year_from,
 	'ignore': '.csv'
-}
-
-payload_one={
-	's': 'VTI'
 }
 
 r = requests.get(base_url, params=payload)
